1900047a5f617f224833de4b88eb1616645fd385
daniel.wisniewski <daniel.wisniewski3@gmail.com>
2021-01-04 12:13:17 +0100

    Tv seances in the evening implementation. Change method of data acquiring after changes in backend. Firestore collection is used insted of REST. Improve way of presenting data in the list.

--- src/app/movies/in-tv-today/evening-seances/evening-seances.component.ts
+++ src/app/movies/in-tv-today/evening-seances/evening-seances.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { FilmData } from 'src/app/shared/Models/film-data.model';
 import { MoviesInTvService } from '../movies-in-tv.service';
 
@@ -7,14 +8,21 @@ import { MoviesInTvService } from '../movies-in-tv.service';
   templateUrl: './evening-seances.component.html',
   styleUrls: ['./evening-seances.component.css'],
 })
-export class EveningSeancesComponent implements OnInit {
+export class EveningSeancesComponent implements OnInit, OnDestroy {
   filmsData: FilmData[];
+  sub: Subscription;
   constructor(private tvService: MoviesInTvService) {}
 
   ngOnInit(): void {
-    this.tvService.getFilmsThisEvening().subscribe((result) => {
-      this.filmsData = result.filter((film) => film.title);
-      console.log(this.filmsData);
-    });
+    this.sub = this.tvService.filmsInEvening.subscribe(
+      (val) => (this.filmsData = val)
+    );
+    this.tvService.fetchFilmsInEvening();
+  }
+
+  ngOnDestroy() {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
   }
 }
